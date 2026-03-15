017d57e341f7ca16eb897868421287595b93654c
Святослав <@DESKTOP-P52PNM6>
2026-03-15 23:10:56 +0300

    Добавьте файлы проекта.

---
+++ n3/Program.cs
@@ -0,0 +1,10 @@
+﻿class TaskManager
+{
+    // Нарушение 1: кривые отступы (прижато к левому краю)
+    private string taskName = "Лаба"; // Нарушение 2: приватное поле без подчеркивания
+
+    public void PrintTask( )
+    {
+        Console.WriteLine(taskName);
+    }
+}
